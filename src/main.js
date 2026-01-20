44bbc67c358b1d9a2a68d9ee37a135a969f4c0de
Noyan67 <noyanbahadirr@outlook.com>
2026-01-20 19:49:21 +0100

    centr game on screen

--- src/main.js
+++ src/main.js
@@ -49,6 +49,10 @@ new Phaser.Game({
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
   backgroundColor: "#1d1d1d",
   physics: {
     default: "arcade",
